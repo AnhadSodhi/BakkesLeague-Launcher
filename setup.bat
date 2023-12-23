180c92e21bd471870c5f13ff4edf19eea1f89e93
Anhad Sodhi <anhadsodhi2005@gmail.com>
2023-12-23 15:20:44 -0800

    Fixed issue where setup.bat would not find file paths if Bakkesmod and Rocket League were in different drives

--- setup.bat
+++ setup.bat
@@ -2,28 +2,31 @@
 setlocal enabledelayedexpansion
 
 set "search_programs=RocketLeague.exe BakkesMod.exe"
-set "search_directory=C:\"
 set "output_batch_file=launcher.bat"
+set "drive_letters=A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
 
 rem Delete the output batch file if it already exists
-if exist "%output_batch_file%" del "%output_batch_file%"
-
-for %%p in (%search_programs%) do (
-	echo Searching for: %%p
-
-	for %%D in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
-		if exist "%%D:\" (
-
-			for /r "%search_directory%" %%f in (%%p) do (
-				if exist "%%f" (
-					echo Found: %%f
-					echo start "" "%%f" >> "%output_batch_file%"
-				)
-			)
-
-		)
-	)
-	
+if exist "%output_batch_file%" (
+	del "%output_batch_file%"
+	echo old launcher.bat deleted.
 )
 
+rem Iterate through all programs
+for %%p in (%search_programs%) do (
+	echo Searching for: %%p
+	
+	rem Iterate through all drives
+	for %%d in (%drive_letters%) do (
+		set "search_directory=%%d:\"
+		
+		rem Use dir /s /b to recursively search for the program path
+		for /f "tokens=*" %%f in ('dir /s /b "!search_directory!%%p" 2^>nul') do (
+			echo Found: %%f
+			echo start "" "%%f" >> "%output_batch_file%"
+		)
+	)
+)
+echo new launcher.bat file created. You may need to refresh the folder to make it appear.
+
 endlocal
+pause
